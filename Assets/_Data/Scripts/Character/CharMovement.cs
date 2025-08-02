084ed2c9013855746b1376170e09cd19f1c3436f
Ca11MeHCL <minhphung755@gmail.com>
2025-08-02 13:11:27 +0700

    Character Jump

--- Assets/_Data/Scripts/Character/CharMovement.cs
+++ Assets/_Data/Scripts/Character/CharMovement.cs
@@ -6,42 +6,41 @@ public class CharMovement : MyMonoBehaviour
     [Header("CharMovement")]
     [SerializeField] protected CharCtrl charCtrl;
     [SerializeField] protected float _moveSpeed = 3f;
-    protected float _xDirection;
-
+    protected float xDirection;
+    protected int jumpCount = 0;
+    protected bool canJump;
+    protected int maxExtraJump = 2;
+    [SerializeField] protected float jumpForce = 6f;
+    //AUTO LOAD
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadCharCtrl();
     }
-
     protected virtual void LoadCharCtrl()
     {
         if (charCtrl != null) return;
         charCtrl = GetComponentInParent<CharCtrl>();
         Debug.LogWarning(transform.name + ": LoadCharCtrl", gameObject);
     }
-
     private void Update()
     {
         this.GetXDirection();
         this.CheckJump();
         this.ResetJumpCount();
     }
-
     private void FixedUpdate()
     {
         this.Move();
         this.Jump();
     }
-
     protected virtual void GetXDirection()
     {
 
-        _xDirection = InputManager.Instance.MoveInput;
-
+        xDirection = InputManager.Instance.MoveInput;
        // if (_xDirection == 0) return;
     }
-
+    //CHARACTER MOVE
     protected virtual void Move()
     {
         //if (_charCtrl.CharState.GetIsDead())
@@ -49,81 +48,58 @@ public class CharMovement : MyMonoBehaviour
         //    _charCtrl.Rigidbody2D.velocity = new Vector2(0, _charCtrl.Rigidbody2D.velocity.y);
         //    return;
         //}
-        float _moveStep = _xDirection * _moveSpeed;
+        float _moveStep = xDirection * _moveSpeed;
         //if (_charCtrl.CharState.WallJumping) return;
         //if (_charCtrl.CharState.Dashing) return;
         charCtrl.RigidBody2D.linearVelocity = new Vector2(_moveStep, charCtrl.RigidBody2D.linearVelocity.y);
 
         this.RunningFlip();
     }
-
     protected virtual void RunningFlip()
     {
-        if (_xDirection != 0)
+        if (xDirection != 0)
         {
             Vector3 scale = charCtrl.transform.localScale;
-            scale.x = Mathf.Abs(scale.x) * (_xDirection < 0 ? -1 : 1);
+            scale.x = Mathf.Abs(scale.x) * (xDirection < 0 ? -1 : 1);
             charCtrl.transform.localScale = scale;
         }
     }
-
     public float GetMoveSpeed()
     {
         return _moveSpeed;
     }
-
     public void SetMoveSpeed(float newSpeed)
     {
         _moveSpeed = newSpeed;
     }
-
-
-
-
-    /// <summary>
-    /// ////
-    /// </summary>
-
-
-    /// <summary>
-    /// //////
-    /// </summary>
-
-     protected int jumpCount;
-     protected bool canJump;
-     protected bool finishJump;
-     protected int maxExtraJump = 2;
-     protected bool _finishJump;
-    [SerializeField] protected float jumpForce = 6f;
-
-
-
-
-
-
-
-
-
+    //CHARACTER JUMP
     protected virtual void Jump()
     {
         if (!canJump) return;
-        if (!charCtrl.CharState.IsGrounded) return;
         this.charCtrl.RigidBody2D.linearVelocity = new Vector2(charCtrl.RigidBody2D.linearVelocity.x, this.jumpForce);
         canJump = !canJump;
         jumpCount++;
     }
-
     protected virtual void CheckJump()
     {
-        if (InputManager.Instance.JumpInput && jumpCount < maxExtraJump) this.canJump = true;
-    }
+        if (!InputManager.Instance.JumpInput) return;
 
+        if (charCtrl.CharState.IsGrounded)
+        {
+            jumpCount = 0;
+            canJump = true;
+        }
+
+        if (canJump || jumpCount < maxExtraJump - 1)
+        {
+            charCtrl.RigidBody2D.linearVelocity = new Vector2(charCtrl.RigidBody2D.linearVelocity.x, jumpForce);
+            jumpCount++;
+            canJump = false;
+        }
+    }
     protected virtual void ResetJumpCount()
     {
-        if (charCtrl.RigidBody2D.linearVelocity.y > 0.1) return;
-        if (charCtrl.RigidBody2D.linearVelocity.y < -0.1) return;
         if (!charCtrl.CharState.IsGrounded) return;
-        if (jumpCount == 0) return;
         jumpCount = 0;
     }
 
