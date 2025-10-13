d7132767e0166a1557ec89c45609775363242d3a
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2025-10-13 21:05:16 +0700

    menu update
    13/10/2025

--- Assets/_Data/Scripts/Character/CharCtrl.cs
+++ Assets/_Data/Scripts/Character/CharCtrl.cs
@@ -2,106 +2,120 @@
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(CapsuleCollider2D))]
-
 public class CharCtrl : MyMonoBehaviour
 {
-    [Header("CharacterCtrl")]
+	[Header("CharacterCtrl")]
 
-    [SerializeField] protected Rigidbody2D rigidBody2D;
-    public Rigidbody2D RigidBody2D => rigidBody2D;
+	[SerializeField] protected Rigidbody2D rigidBody2D;
+	public Rigidbody2D RigidBody2D => rigidBody2D;
 
-    [SerializeField] protected CapsuleCollider2D charBodyCollider;
-    public CapsuleCollider2D CharBodyCollider => charBodyCollider;
+	[SerializeField] protected CapsuleCollider2D charBodyCollider;
+	public CapsuleCollider2D CharBodyCollider => charBodyCollider;
 
-    [SerializeField] protected CharStateCtrl charStateCtrl;
-    public CharStateCtrl CharStateCtrl => charStateCtrl;
+	[SerializeField] protected CharStateCtrl charStateCtrl;
+	public CharStateCtrl CharStateCtrl => charStateCtrl;
 
-    [SerializeField] protected CharData charData;
-    public CharData CharData => charData;
+	[SerializeField] protected CharData charData;
+	public CharData CharData => charData;
 
-    [SerializeField] protected EnvironmentChecker environmentChecker;
-    public EnvironmentChecker EnvironmentChecker => environmentChecker;
+	[SerializeField] protected EnvironmentChecker environmentChecker;
+	public EnvironmentChecker EnvironmentChecker => environmentChecker;
 
-    [SerializeField] protected AnimationCtrl animationCtrl;
-    public AnimationCtrl AnimationCtrl => animationCtrl;
+	[SerializeField] protected AnimationCtrl animationCtrl;
+	public AnimationCtrl AnimationCtrl => animationCtrl;
 
-    [SerializeField] protected PointCtrl pointCtrl;
-    public PointCtrl PointCtrl => pointCtrl;
+	[SerializeField] protected PointCtrl pointCtrl;
+	public PointCtrl PointCtrl => pointCtrl;
 
-    [SerializeField] protected CharDamageSender charDamageSender;
-    public CharDamageSender CharDamageSender => charDamageSender;
+	[SerializeField] protected CharDamageSender charDamageSender;
+	public CharDamageSender CharDamageSender => charDamageSender;
 
-    protected override void LoadComponents()
-    {
-        base.LoadComponents();
-        this.LoadRigidbody2D();
-        this.LoadCharBodyCollider();
-        this.LoadCharData();
-        this.LoadCharStateCtrl();
-        this.LoadEnvironmentChecker();
-        this.LoadAnimationCtrl();
-        this.LoadPointCtrl();
-        this.LoadCharDamageSender();
-    }
+	// --- Thêm Particle Controller ---
+	[SerializeField] protected ParticleEffectsController particleEffectsController;
+	public ParticleEffectsController ParticleEffectsController => particleEffectsController;
 
-    protected virtual void LoadRigidbody2D()
-    {
-        if (rigidBody2D != null) return;
-        this.rigidBody2D = GetComponent<Rigidbody2D>();
-        this.rigidBody2D.gravityScale = this.charData.GravityScale;
-        this.rigidBody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+	// ================================
+	protected override void LoadComponents()
+	{
+		base.LoadComponents();
+		this.LoadRigidbody2D();
+		this.LoadCharBodyCollider();
+		this.LoadCharData();
+		this.LoadCharStateCtrl();
+		this.LoadEnvironmentChecker();
+		this.LoadAnimationCtrl();
+		this.LoadPointCtrl();
+		this.LoadCharDamageSender();
+		this.LoadParticleEffectsController(); // <- thêm vào đây
+	}
 
-        Debug.Log(transform.name + ": LoadRigidbody2D", gameObject);
-    }
-    protected virtual void LoadCharBodyCollider()
-    {
-        if (charBodyCollider != null) return;
-        this.charBodyCollider = GetComponent<CapsuleCollider2D>();
-        this.charBodyCollider.offset = new Vector2(0f, 0f);        
-        this.charBodyCollider.size = new Vector2(0.5f, 1.12f);         
-        this.charBodyCollider.isTrigger = false;                                  
-        Debug.Log(transform.name + ": LoadCharBodyCollider", gameObject);
-    }
+	protected virtual void LoadRigidbody2D()
+	{
+		if (rigidBody2D != null) return;
+		this.rigidBody2D = GetComponent<Rigidbody2D>();
+		this.rigidBody2D.gravityScale = this.charData.GravityScale;
+		this.rigidBody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
 
-    protected virtual void LoadCharData()
-    {
-        if (charData != null) return;
-        charData = GetComponentInChildren<CharData>();
-        Debug.LogWarning(transform.name + ": LoadCharData", gameObject);
-    }
+		Debug.Log(transform.name + ": LoadRigidbody2D", gameObject);
+	}
 
-    protected virtual void LoadCharStateCtrl()
-    {
-        if (charStateCtrl != null) return;
-        charStateCtrl = GetComponentInChildren<CharStateCtrl>();
-        Debug.LogWarning(transform.name + ": LoadCharStateCtrl", gameObject);
-    }
+	protected virtual void LoadCharBodyCollider()
+	{
+		if (charBodyCollider != null) return;
+		this.charBodyCollider = GetComponent<CapsuleCollider2D>();
+		this.charBodyCollider.offset = new Vector2(0f, 0f);
+		this.charBodyCollider.size = new Vector2(0.5f, 1.12f);
+		this.charBodyCollider.isTrigger = false;
+		Debug.Log(transform.name + ": LoadCharBodyCollider", gameObject);
+	}
 
-    protected virtual void LoadEnvironmentChecker()
-    {
-        if (environmentChecker != null) return;
-        environmentChecker = GetComponentInChildren<EnvironmentChecker>();
-        Debug.LogWarning(transform.name + ": LoadEnvironmentChecker", gameObject);
-    }
+	protected virtual void LoadCharData()
+	{
+		if (charData != null) return;
+		charData = GetComponentInChildren<CharData>();
+		Debug.LogWarning(transform.name + ": LoadCharData", gameObject);
+	}
 
-    protected virtual void LoadAnimationCtrl()
-    {
-        if (animationCtrl != null) return;
-        animationCtrl = GetComponentInChildren<AnimationCtrl>();
-        Debug.LogWarning(transform.name + ": LoadAnimationCtrl", gameObject);
-    }
+	protected virtual void LoadCharStateCtrl()
+	{
+		if (charStateCtrl != null) return;
+		charStateCtrl = GetComponentInChildren<CharStateCtrl>();
+		Debug.LogWarning(transform.name + ": LoadCharStateCtrl", gameObject);
+	}
 
-    protected virtual void LoadPointCtrl()
-    {
-        if (pointCtrl != null) return;
-        pointCtrl = GetComponentInChildren<PointCtrl>();
-        Debug.LogWarning(transform.name + ": LoadPointCtrl", gameObject);
-    }
+	protected virtual void LoadEnvironmentChecker()
+	{
+		if (environmentChecker != null) return;
+		environmentChecker = GetComponentInChildren<EnvironmentChecker>();
+		Debug.LogWarning(transform.name + ": LoadEnvironmentChecker", gameObject);
+	}
 
-    protected virtual void LoadCharDamageSender()
-    {
-        if (charDamageSender != null) return;
-        charDamageSender = GetComponentInChildren<CharDamageSender>();
-        Debug.LogWarning(transform.name + ": LoadCharDamageSender", gameObject);
-    }
+	protected virtual void LoadAnimationCtrl()
+	{
+		if (animationCtrl != null) return;
+		animationCtrl = GetComponentInChildren<AnimationCtrl>();
+		Debug.LogWarning(transform.name + ": LoadAnimationCtrl", gameObject);
+	}
+
+	protected virtual void LoadPointCtrl()
+	{
+		if (pointCtrl != null) return;
+		pointCtrl = GetComponentInChildren<PointCtrl>();
+		Debug.LogWarning(transform.name + ": LoadPointCtrl", gameObject);
+	}
+
+	protected virtual void LoadCharDamageSender()
+	{
+		if (charDamageSender != null) return;
+		charDamageSender = GetComponentInChildren<CharDamageSender>();
+		Debug.LogWarning(transform.name + ": LoadCharDamageSender", gameObject);
+	}
+
+	// --- Hàm load thêm Particle Controller ---
+	protected virtual void LoadParticleEffectsController()
+	{
+		if (particleEffectsController != null) return;
+		particleEffectsController = GetComponentInChildren<ParticleEffectsController>();
+		Debug.LogWarning(transform.name + ": LoadParticleEffectsController", gameObject);
+	}
 }
---
+++ Assets/_Data/Scripts/Menu/MenuHoverSmooth.cs
@@ -0,0 +1,60 @@
+// MenuHoverSmooth.cs
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuHoverSmooth : MyMonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[SerializeField] protected TextMeshProUGUI text;
+	public TextMeshProUGUI Text => text;
+	public Color normalColor = Color.white;
+	public Color hoverColor = Color.yellow;
+	public float duration = 0.12f;
+
+	private Coroutine running;
+	protected override void LoadComponents()
+	{
+		base.LoadComponents();
+		this.LoadTextMesh();
+	}
+	void Start()
+	{
+		if (text != null) text.color = normalColor;
+	}
+	protected virtual void LoadTextMesh()
+	{
+		if (text != null) return;
+		text = GetComponentInChildren<TextMeshProUGUI>();
+		Debug.LogWarning(transform.name + ": LoadTextMesh", gameObject);
+	}
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		StartColorTween(hoverColor);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		StartColorTween(normalColor);
+	}
+
+	void StartColorTween(Color target)
+	{
+		if (running != null) StopCoroutine(running);
+		running = StartCoroutine(ColorTween(text.color, target));
+	}
+
+	IEnumerator ColorTween(Color from, Color to)
+	{
+		float t = 0f;
+		while (t < duration)
+		{
+			t += Time.unscaledDeltaTime; // UI should ignore timescale
+			if (text != null) text.color = Color.Lerp(from, to, t / duration);
+			yield return null;
+		}
+		if (text != null) text.color = to;
+		running = null;
+	}
+}
+
