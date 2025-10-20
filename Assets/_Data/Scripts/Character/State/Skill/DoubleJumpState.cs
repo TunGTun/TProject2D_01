dd0a0ded99addcaed3170248e10436e8e9e82ff5
Bui-toan <145842406+Bui-toan@users.noreply.github.com>
2025-10-20 21:09:29 +0700

    ve map above
    20/10/2025

--- Assets/_Data/Scripts/Character/State/Skill/DoubleJumpState.cs
+++ Assets/_Data/Scripts/Character/State/Skill/DoubleJumpState.cs
@@ -2,60 +2,60 @@ using UnityEngine;
 
 public class DoubleJumpState : ICharState<CharBaseState>
 {
-    public string Name => StateName.JUMP_STATE;
+	public string Name => StateName.JUMP_STATE;
 
-    public FSMType FSMType => FSMType.Skill;
+	public FSMType FSMType => FSMType.Skill;
 
-    private Transform fx;
+	private Transform fx;
 
-    //protected int jumpCount = 0;
+	//protected int jumpCount = 0;
 
-    public void OnEnter(CharBaseState context)
-    {
-        if (!context.CharCtrl.CharStateCtrl.SkillLock.IsUnlocked(ESkill.DoubleJump))
-        {
-            context.CharCtrl.CharStateCtrl.SkillState.ChangeState(
-                context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
-            return;
-        }
-        
-        if (!context.CharCtrl.CharStateCtrl.canDoubleJump)
-        {
-            context.CharCtrl.CharStateCtrl.SkillState.ChangeState(
-                context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
-            return;
-        }
-        
-        context.CharCtrl.CharStateCtrl.canDoubleJump = false;
+	public void OnEnter(CharBaseState context)
+	{
+		if (!context.CharCtrl.CharStateCtrl.SkillLock.IsUnlocked(ESkill.DoubleJump))
+		{
+			context.CharCtrl.CharStateCtrl.SkillState.ChangeState(
+				context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
+			return;
+		}
 
-        context.CharCtrl.RigidBody2D.linearVelocity = new Vector2(context.CharCtrl.RigidBody2D.linearVelocity.x, 0);
-        context.CharCtrl.RigidBody2D.AddForce(Vector2.up * context.CharCtrl.CharData.DoubleJumpForce, ForceMode2D.Impulse);
+		if (!context.CharCtrl.CharStateCtrl.canDoubleJump)
+		{
+			context.CharCtrl.CharStateCtrl.SkillState.ChangeState(
+				context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
+			return;
+		}
 
-        context.CharCtrl.AnimationCtrl.UpdateAnimation();
+		context.CharCtrl.CharStateCtrl.canDoubleJump = false;
 
-        Vector3 spawnPos = context.CharCtrl.AnimationCtrl.transform.position;
-        Quaternion spawnRot = Mathf.Approximately(context.CharCtrl.transform.localScale.x, 1) ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
-        fx = FXSpawner.Instance.Spawn(FXSpawner.Instance.DOUBLE_JUMP, spawnPos, spawnRot);
-    }
+		context.CharCtrl.RigidBody2D.linearVelocity = new Vector2(context.CharCtrl.RigidBody2D.linearVelocity.x, 0);
+		context.CharCtrl.RigidBody2D.AddForce(Vector2.up * context.CharCtrl.CharData.DoubleJumpForce, ForceMode2D.Impulse);
 
-    public void OnExit(CharBaseState context)
-    {
-        FXSpawner.Instance.Despawn(fx);
-    }
+		context.CharCtrl.AnimationCtrl.UpdateAnimation();
 
-    public void OnFrameUpdate(CharBaseState context)
-    {
-        if (context.CharCtrl.RigidBody2D.linearVelocityY <= 0)
-        {
-            context.CharCtrl.CharStateCtrl.SkillState.ChangeState(context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
-        }
-        
-        fx.position = context.CharCtrl.AnimationCtrl.transform.position;
-        fx.rotation = Mathf.Approximately(context.CharCtrl.transform.localScale.x, 1) ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
-    }
+		Vector3 spawnPos = context.CharCtrl.AnimationCtrl.transform.position;
+		Quaternion spawnRot = Mathf.Approximately(context.CharCtrl.transform.localScale.x, 1) ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+		fx = FXSpawner.Instance.Spawn(FXSpawner.Instance.DOUBLE_JUMP, spawnPos, spawnRot);
+	}
 
-    public void OnPhysicUpdate(CharBaseState context)
-    {
+	public void OnExit(CharBaseState context)
+	{
+		/* FXSpawner.Instance.Despawn(fx);*/
+	}
 
-    }
+	public void OnFrameUpdate(CharBaseState context)
+	{
+		if (context.CharCtrl.RigidBody2D.linearVelocityY <= 0)
+		{
+			context.CharCtrl.CharStateCtrl.SkillState.ChangeState(context.CharCtrl.CharStateCtrl.SkillState.idleSkill);
+		}
+
+		fx.position = context.CharCtrl.AnimationCtrl.transform.position;
+		fx.rotation = Mathf.Approximately(context.CharCtrl.transform.localScale.x, 1) ? Quaternion.identity : Quaternion.Euler(0, 180, 0);
+	}
+
+	public void OnPhysicUpdate(CharBaseState context)
+	{
+
+	}
 }
